6a8ebb3e5a05bdd305c375cb6dd78ec51ed853f3
Humberto <hdkaratechop@gmail.com>
2022-07-20 16:52:49 -0700

    Add comments for self.speed and check functionality

--- car_app.rb
+++ car_app.rb
@@ -7,9 +7,15 @@ class MyCar
         @model = model
         @speed = speed
     end
-
-    def change_speed(speed)
-        return speed += speed
+        # We need to use self.speed to let Ruby know that we are calling a method,
+        # not the instance variable @speed. self.speed = speed and @speed = speed are different.
+        # the former makes a method call to stuff= on the object, ] whereas the latter directly 
+        # sets an instance variable.The former invokes a method which may be public (unless 
+        # specifically declared private in the class), whereas the latter is always setting a 
+        # private instance variable.
+    def change_speed(x)
+        self.speed = x 
+        return self.speed
     end
 
     def shutt_off()
@@ -18,6 +24,7 @@ class MyCar
 
     def spray_paint(color)
         self.color = color 
+        return "You car is now #{color}"
     end
 
     def color()
@@ -32,3 +39,7 @@ puts my_whip
 puts my_whip.color 
 
 puts my_whip.change_speed(20)
+
+puts my_whip.spray_paint('orange')
+
+puts my_whip.color
